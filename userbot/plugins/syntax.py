aa13b3241a68a1f07aed64da274fe1a224d49a10
TechyNewbie <63805243+Techy05@users.noreply.github.com>
2020-05-21 00:18:20 +0530

    Use .modules and .syntax for help

--- userbot/plugins/syntax.py
+++ userbot/plugins/syntax.py
@@ -5,7 +5,7 @@ from telethon import events
 from userbot.utils import admin_cmd
 import asyncio
 from telethon.tl import functions, types
-from global_variables_sql import SYNTAX, MODULE_LIST
+from sql.global_variables_sql import SYNTAX, MODULE_LIST
 
 
 MODULE_LIST.append("syntax")
