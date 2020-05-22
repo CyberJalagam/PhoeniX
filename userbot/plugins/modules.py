efd039ed2b617fe93b49f51c96c3bdf5554affa8
TechyNewbie <63805243+Techy05@users.noreply.github.com>
2020-05-22 22:27:01 +0530

    Update modules.py

--- userbot/plugins/modules.py
+++ userbot/plugins/modules.py
@@ -13,7 +13,7 @@ from sql.global_variables_sql import MODULE_LIST
 async def _(event):
     if event.fwd_from:
         return
-    modules = "**List of available modules:**\n"
+    modules = "💫  **List of available modules in PhoeniX**\n"
     MODULE_LIST.sort()
     prev = "1"
     num = 0
@@ -21,7 +21,7 @@ async def _(event):
         num += 1
         if prev[0] != module[0]:
             modules += f"\n\t{module[0].upper()}\n"
-        modules += f"~ ```{module}```\n"
+        modules += f"-  ```{module}```\n"
         prev = module
-    modules += f"**\n\nNumber of modules loaded:** {num}\n**Tip: Use .syntax <module_name> for more info.**"
+    modules += f"\n\n__Number of modules loaded:__ {num}\n**Tip:** Use .syntax <module_name> for more info."
     await event.edit(modules)
