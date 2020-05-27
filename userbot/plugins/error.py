aec082eb5ca151806def8134142aead6f7a39dea
TechyNewbie <63805243+Techy05@users.noreply.github.com>
2020-05-27 15:51:52 +0530

    Two-step forward :)

---
+++ userbot/plugins/error.py
@@ -0,0 +1,24 @@
+# Ported from Priyam's The TG Bot 2.0
+# A clean and beautiful alternative to .help
+# Syntax (.error <error_name>)
+from telethon import events
+from userbot.utils import admin_cmd
+import asyncio
+from telethon.tl import functions, types
+from sql.global_variables_sql import SYNTAX, MODULE_LIST, ERROR, ERRORS
+
+
+MODULE_LIST.append("error")
+
+@borg.on(admin_cmd(pattern="error ?(.*)"))
+async def _(event):
+        if event.fwd_from:
+            return
+        err = event.pattern_match.group(1)
+        if err:
+            if err in ERROR:
+                await event.edit(ERROR[err])
+            else:
+                await event.edit("Please specify a real error.")
+        else:
+            await event.edit("Please specify an error.\n**Tip: Get a list of all errors using .errors**")
