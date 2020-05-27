055dffb5840c4b508b3fdc3c1ea4838ac46fd2d6
TechyNewbie <63805243+Techy05@users.noreply.github.com>
2020-05-27 20:10:45 +0530

    Fixed crash in error_info.py

--- userbot/plugins/error_info.py
+++ userbot/plugins/error_info.py
@@ -21,8 +21,8 @@ ERROR.update({
     "updater not working": "\
 **So, you're having problems with updater. PhoeniX Service will fix it.**\
 \n\nERROR: `Unfortunately, the directory /app does not seem to be a git repository.`\
-\nSolution: __Use ".update now" and check again if it works.__\
-\n__If it still doesn't work, then use ".chl" once.\
+\nSolution: __Use “.update now” and check again if it works.__\
+\n__If it still doesn't work, then use “.chl” once.\
 \n\nERROR: `[UPDATER]: Looks like you are using your own custom branch (master). in that case, Updater is unable to identify which branch is to be merged. please checkout to any official branch`\
 \nSolution: __Delete PhoeniX repo from your account. Refork__ [PhoeniX](https://github.com/Techy05/PhoeniX). __Then Manual Deploy from Heroku to fix__\
 \nIf you use custom fork, then please don't mess with branches.\
