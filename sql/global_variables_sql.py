56cf547ecfefc3f3e76a2e957fb4372c680eb383
TechyNewbie <63805243+Techy05@users.noreply.github.com>
2020-05-27 15:35:07 +0530

    error support coming soon :)

--- sql/global_variables_sql.py
+++ sql/global_variables_sql.py
@@ -8,6 +8,10 @@ PACKS = Var.PACKS_CONTENT
 DL = Var.TEMP_DOWNLOAD_DIRECTORY
 # add modules to this list using MODULES_LIST.append(MODULE_NAME)
 MODULE_LIST = []
+# add error to this list using ERROR.append(ERROR_NAME)
+ERROR = []
 # add syntax to this dictionary using SYNTAX.update()
 SYNTAX = {}
+# add errors to this dictionary using ERRORS.update()
+ERRORS = {}
 BUILD = "USER-49x03"
