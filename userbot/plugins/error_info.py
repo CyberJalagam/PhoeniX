99022ea95ff5bedbc3c4a7fbe06eec12eb875c54
TechyNewbie <63805243+Techy05@users.noreply.github.com>
2020-05-29 18:10:28 +0530

    Fixed formatting

--- userbot/plugins/error_info.py
+++ userbot/plugins/error_info.py
@@ -11,7 +11,7 @@ SYNTAX.update({
 \nDon't worry, Let's fix them\
 \n\n• `.errors`\
 \nUsage: __Shows the list of known errors in Userbots__\
-\n\n• `.error <error_name>`\
+\n\n• `.solution <error_name>`\
 \nUsage: __Shows the solution for the requested error/problem__\
 "
 })
@@ -22,7 +22,7 @@ ERROR.update({
 **So, you're having problems with updater. PhoeniX Service will fix it.**\
 \n\nERROR: `Unfortunately, the directory /app does not seem to be a git repository.`\
 \nSolution: __Use “.update now” and check again if it works.__\
-\n__If it still doesn't work, then use “.chl” once.\
+\n__If it still doesn't work, then use “.chl” once.__\
 \n\nERROR: `[UPDATER]: Looks like you are using your own custom branch (master). in that case, Updater is unable to identify which branch is to be merged. please checkout to any official branch`\
 \nSolution: __Delete PhoeniX repo from your account. Refork__ [PhoeniX](https://github.com/Techy05/PhoeniX). __Then Manual Deploy from Heroku to fix__\
 \nIf you use custom fork, then please don't mess with branches.\
