14e0b65499b89fa50cca8dcfa07d9bef28aaa388
TechyNewbie <63805243+Techy05@users.noreply.github.com>
2020-05-29 17:27:17 +0530

    Fixed 'dict' #3

--- userbot/plugins/errors.py
+++ userbot/plugins/errors.py
@@ -7,18 +7,18 @@ from telethon import events
 from userbot.utils import admin_cmd
 import asyncio
 from telethon.tl import functions, types
-from sql.global_variables_sql import ERRORS
+from sql.global_variables_sql import ERROR_LIST
 
 
 @borg.on(admin_cmd(pattern="errors ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
-    erros = "💫  **List of known errors in Userbots**\n"
-    ERRORS.sort()
+    erros = "💫  **List of known errors in Userbots (for noobs)**\n"
+    ERROR_LIST.sort()
     prev = "1"
     num = 0
-    for err in ERRORS:
+    for err in ERROR_LIST:
         num += 1
         if prev[0] != err[0]:
             erros += f"\n\t{err[0].upper()}\n"
